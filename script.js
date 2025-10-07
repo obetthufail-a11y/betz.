111562f0ae53777b6f8aeabb9969903d0dc4038e
bet.z00 <obetthufail@gmail.com>
2025-10-07 12:12:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombol sapa = document.getElementById['sapaButton']:
+const tombolSapa = document.getElementById['sapaButton']:
 tombolSapa.addEventListenener['click'.funcution[]{
 alert['hallo terima kasih sudah berkunjung!']:
 }];
